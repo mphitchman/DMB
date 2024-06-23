b538816b2003157a97024a892cccdff5cb5c0b9e
Mike Hitchman <hitchmp@gmail.com>
2024-06-23 06:29:33 -0700

    update pit24 and hit24

---
+++ main_scrape.py
@@ -0,0 +1,51 @@
+import pybaseball as pyb
+import pandas as pd
+
+def load_key():
+    return(pd.read_csv("https://mphitchman.com/DMB/csv/keyID.csv"))
+
+def load_hit():
+    df = pyb.fg_batting_data(start_season=2024,qual=10)
+    df = df.rename(columns={"IDfg": "key_FG","Team":"MLB"})
+    df = df.merge(keyID[keyID['type']=="B"][['type','key_FG','RJML','SSBL','CJPL']],on="key_FG",how='left').set_index('Name')
+    return(df)
+
+def load_fielding():
+    return(pyb.fielding_stats(start_season='2024',qual=3))
+   
+
+def load_pit():
+    pf = pyb.pitching_stats_bref() #baseball reference scrape to get stats needed for ops and rc
+    pf = pf.rename(columns={"mlbID": "key_MLB"})
+    pf["key_MLB"] = pd.to_numeric(pf["key_MLB"])
+    df = pyb.fg_pitching_data(start_season=2024,qual=10)
+    df = df.rename(columns={"IDfg": "key_FG","Team":"MLB"})
+    df = df.merge(keyID[keyID['type']=="P"][['type','key_FG','key_MLB','RJML','SSBL','CJPL']],on="key_FG",how='left')
+    df = df.merge(pf[['BF','AB','2B','3B','SF','GDP','SB','CS','key_MLB']],on='key_MLB',how='left')
+    return(df.set_index('Name'))
+
+def add_O(df):
+    df['O'] = df['H']+df['2B']+2*df['3B']+2*df['HR']+df['BB']+df['SB']+df['SH']+df['R'] + df['RBI']-df['CS']-df['GDP']
+    return(df)
+
+def IP_to_Inn(x):
+    import numpy as np
+    return(np.floor(x)+10*(x-np.floor(x))/3)
+
+def positions_played(key_FG):
+    '''assumes you've defined the dataframe 'fld' via fld = load_fielding()'''
+    df = fld[fld['IDfg'] == key_FG].sort_values(by="Inn",ascending=False)
+    df['Pos'].tolist()
+    df['Pos'] = df['Pos'].fillna('')
+    return(",".join(df['Pos'].tolist())) 
+
+#### scrape
+keyID=load_key()
+hit24 = add_O(load_hit())
+fld = load_fielding()
+hit24['Pos'] = hit24['key_FG'].apply(positions_played)
+pit24 = load_pit()
+pit24['Inn'] = pit24['IP'].apply(IP_to_Inn)
+hit24.to_csv("csv/hit24.csv")
+pit24.to_csv("csv/pit24.csv")
+print("hit24 and pit24 updated and saved to csv folder")
