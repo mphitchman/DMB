a17b955ad76d0f86fcfb51f385ef75bf5640321a
Mike Hitchman <hitchmp@gmail.com>
2024-08-02 15:50:53 +0000

    first working gl

--- app_gl.py
+++ app_gl.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import random
-import plotly.express as px
 from bs4 import BeautifulSoup
 import requests 
 import re
-from datetime import datetime
+import datetime as dt
 from io import StringIO
 
 st.set_page_config(layout="wide")
@@ -37,16 +34,28 @@ def rates(df):
     df['k%'] = round(df['SO']/df['PA']*100,1)
     return(df)
 
+def rates_pit(df):
+    '''df must have these columns: ['IP','BF','AB','H','2B','3B','HR','ER','BB','SO','HBP','SF']'''
+    import numpy as np
+    df['Inn'] = np.floor(df['IP'])+10*(df['IP']-np.floor(df['IP']))/3
+    df['avg'] = round(df['H']/df['AB'],3)
+    df['obp'] = round((df['H']+df['BB']+df['HBP'])/(df['AB']+df['BB']+df['HBP']+df['SF']),3)
+    df['slg'] = round((df['H']+df['2B']+2*df['3B']+3*df['HR'])/df['AB'],3)
+    df['ops'] = df['obp']+df['slg']
+    df['k%'] = round(df['SO']/df['BF']*100,1)
+    df['bb%'] = round(df['BB']/df['BF']*100,1)
+    df['hr9'] = round(df['HR']/df['Inn']*9,1)
+    df['whip'] = round((df['BB']+df['H'])/df['Inn'],2)
+    df['era'] = round(df['ER']/df['Inn']*9,2)
+    df['babip'] = round((df['H']-df['HR'])/(df['AB']-df['HR']-df['SO']+df['SF']),3)
+    df['k-bb%'] = df['k%']-df['bb%']
+    return(df)
+
+
+
 hit_columns = ['Name','O','PA','AB','H','2B','3B','HBP','SF','HR','R','RBI','BB','SO','SB']
 
-def team_log(tm,lg):
-    table = df[df[lg]==tm][hit_columns]
-    table.loc['Total'] = table.sum()
-    table.loc[table.index[-1], 'Name'] = 'Total'
-    table = rates(table)[['Name','O','PA','H','2B','3B','HR','R','RBI','SB','BB','SO','avg','obp','slg','ops']]
-    return(table)
-
-def load_gamelog():
+def load_hit_gamelog():
     date = dt.datetime.today().strftime('%Y-%m-%d')
     st_date = dt.datetime.today().replace(day=1).strftime('%Y-%m-%d')
     game_date = (dt.datetime.today()+dt.timedelta(days=-1)).strftime('%Y-%m-%d')
@@ -86,8 +95,72 @@ def load_gamelog():
     keyID = pd.read_csv('csv/keyID.csv')
     df = df.merge(keyID[keyID['type']=="B"][['type','key_MLB','RJML','SSBL','CJPL']],on="key_MLB",how='left')
     return(df)
-    
-df = load_gamelog()
+   
+hf = load_hit_gamelog()
+
+def team_hit_log(tm,lg):
+    table = hf[hf[lg]==tm][hit_columns]
+    table.loc['Total'] = table.sum()
+    table.loc[table.index[-1], 'Name'] = 'Total'
+    table = rates(table)[['Name','O','PA','H','2B','3B','HR','R','RBI','SB','BB','SO','avg','obp','slg','ops']]
+    return(table)
+
+def load_pit_gamelog():
+    date = dt.datetime.today().strftime('%Y-%m-%d')
+    st_date = dt.datetime.today().replace(day=1).strftime('%Y-%m-%d')
+    game_date = (dt.datetime.today()+dt.timedelta(days=-1)).strftime('%Y-%m-%d')
+    url = "https://www.baseball-reference.com/leagues/daily.fcgi?request=1&type=p&dates=yesterday&lastndays=7&since=2024-08-01&fromandto=2024-08-01.2024-08-31&level=mlb&franch=ANY"
+    response = requests.get(url)
+    html = response.text
+    # Create the soup object
+    soup = BeautifulSoup(html, "lxml")
+    # extract the table
+    table = soup.find_all('table', {'class': 'sortable stats_table'})[0]
+    headers = []
+    rows = []
+    for i, row in enumerate(table.find_all('tr')):
+        if i == 0:
+            headers = [el.text.strip() for el in row.find_all('th')]
+        else:
+            rows.append([el.text.strip() for el in row.find_all('td')])
+    # create and clean the dataframe
+    df = pd.read_html(StringIO(str(table)))[0]
+    df = df[df['Name'] != 'Name']
+    num_cols = ['GS', 'W', 'L', 'SV', 'IP', 'H', 'R', 'ER', 'BB','SO', 'HR', 'HBP', 'GSc','AB','2B','3B','IBB','GDP','SF','SB','CS', 'PO','BF']
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)
+    df.drop(['Rk','Unnamed: 2', 'Unnamed: 3', 'Lev'], axis=1, inplace=True)
+    df = df.rename(columns = {'Unnamed: 7':'H/A'})
+    df['H/A']=df['H/A'].fillna(' ')
+    links = table.find_all('a', href=True)
+    m = int(len(links)/3)
+    key_MLB = [""]*m
+    for k in range(m):
+        i = 3*k
+        key_MLB[k] = int(extract_id(links[i]['href'])) 
+    df['key_MLB'] = key_MLB
+    df['date'] = game_date
+    keyID = pd.read_csv('csv/keyID.csv')
+    df = df.merge(keyID[keyID['type']=="P"][['type','key_MLB','RJML','SSBL','CJPL']],on="key_MLB",how='left')
+    return(df)
+
+pf = load_pit_gamelog()
+
+
+def team_pit_log(tm,lg):
+    pit_columns = ['Name','H/A','Opp','GS','W','L','SV','IP','BF','AB','H','2B','3B','HR','ER','BB','SO','HBP','SF']
+    table = pf[pf[lg]==tm][pit_columns]
+    if table.shape[0]==0:
+        tbl_return = table[['Name','GS','Opp']]
+    else:
+        table.loc['Total'] = table.sum()
+        table.loc[table.index[-1], 'Name'] = 'Total'
+        table.loc[table.index[-1], 'H/A'] = '-'
+        table.loc[table.index[-1], 'Opp'] = '-'
+        table = rates_pit(table)
+        x = table.loc[table.index[-1],'Inn']
+        table.loc[table.index[-1],'IP'] = int((x-int(x))/.333)/10+int(x)
+        tbl_return = table[['Name','H/A','Opp','GS','IP','H','ER','BB','HR','SO','avg','obp','slg','era','whip']]
+    return(tbl_return)
 
 ######
 
@@ -97,6 +170,8 @@ df = load_gamelog()
 
 date = dt.datetime.today().strftime('%Y-%m-%d')
 game_date = (dt.datetime.today()+dt.timedelta(days=-1)).strftime('%Y-%m-%d')
+hit_num_cols = ['PA','AB','H','2B','3B','HR','R','RBI','SB','CS','BB','IBB','HBP','SO','SH','SF','GDP']
+pit_num_cols = ['GS', 'W', 'L', 'SV', 'IP', 'H', 'R', 'ER', 'BB','SO', 'HR', 'HBP', 'GSc','AB','2B','3B','IBB','GDP','SF','SB','CS', 'PO','BF']
 
 selected_league = st.selectbox(label='Select a DMB League',options = ['RJML','SSBL','CJPL'],index=0)
 
@@ -111,7 +186,7 @@ st.header("Yesterday's Hitting Game Logs for "+selected_league+" Teams",divider 
 
 
 #get nice teams list for selectboxes
-teams = hit24[hit24[selected_league].notna()][selected_league].unique().tolist()
+teams = keyID[keyID[selected_league].notna()][selected_league].unique().tolist()
 teams.sort()
 teams.remove('avail')
 teams = teams+['Team Totals']
@@ -126,15 +201,16 @@ with col1:
     # st.write("To check for updates, click button at bottom of page, then reload browser.)")
 
     if selected_team == "Team Totals":
-        team_hit_gl = rates(df[[lg]+num_cols].groupby(lg).sum())[['O','PA','H','HR','R','RBI','BB','SO','SB','avg','obp','slg','ops']].sort_values(by=["ops",'O','HR','R'],ascending=False)
-        
+        team_hit_gl = rates(hf[[selected_league]+hit_num_cols].groupby(selected_league).sum())[['O','PA','H','HR','R','RBI','BB','SO','SB','avg','obp','slg','ops']].sort_values(by=["ops",'O','HR','R'],ascending=False)
+        team_pit_gl = rates_pit(pf[[selected_league]+pit_num_cols].groupby(selected_league).sum())[['GS','Inn','H','ER','BB','SO','HR','avg','obp','slg','era','whip']].sort_values(by=["SO"],ascending=False)
     elif selected_team != "Team Totals":
-        team_hit_gl = team_log(lg=selected_league,tm=selected_team)
+        team_hit_gl = team_hit_log(lg=selected_league,tm=selected_team)
+        team_pit_gl = team_pit_log(lg=selected_league,tm=selected_team)
         
 with col2:
     tab1,tab2 = st.tabs(['Hitting','Pitching'])
     with tab1:
-        st.header(selected_team+' - Hitting')
+        st.header(selected_team+' - Hitting on '+game_date)
         st.dataframe(
             team_hit_gl,
             column_config={
@@ -145,9 +221,16 @@ with col2:
             }
         )
     with tab2:
-        st.header(selected_team+' - Pitching')
+        st.header(selected_team+' - Pitching on '+game_date)
         st.dataframe(
-            team_pit_gl = pd.DataFrame({'x':['a','b','c']}),
+            team_pit_gl,
+            column_config={
+                "avg": st.column_config.NumberColumn(format="%.3f"),
+                "obp": st.column_config.NumberColumn(format="%.3f"),
+                "slg": st.column_config.NumberColumn(format="%.3f"),
+                "whip": st.column_config.NumberColumn(format="%.2f"),
+                "era": st.column_config.NumberColumn(format="%.2f"),
+            }
         )  
 
 #if st.button("Clear cache"):
